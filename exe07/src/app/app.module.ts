b7511f06e6c4ed56114f4555da382e552010a8a2
gilles-bertrand <gilles@triptyk.eu>
2018-02-09 16:46:40 +0100

    add last exercice

--- exe07/src/app/app.module.ts
+++ exe07/src/app/app.module.ts
@@ -6,15 +6,28 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NavComponent } from './nav/nav.component';
+import { ProductsComponent } from './products/products.component';
+
+import {
+  routes as childRoutes,
+  ProductsModule
+} from './products/products.module';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent, children: childRoutes },
   { path: 'about', component: AboutComponent }
 ];
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, NavComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavComponent,
+    ProductsComponent
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes), ProductsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
