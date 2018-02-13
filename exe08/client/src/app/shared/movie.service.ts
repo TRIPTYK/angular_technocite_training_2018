f8b398d66f65e446f01408f5b8ba791195e7331f
gilles-bertrand <gilles@triptyk.eu>
2018-02-13 11:52:45 +0100

    add server correections and client start

---
+++ exe08/client/src/app/shared/movie.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+const BASE_URL = 'http://localhost:9000/api/movies';
+const HEADER = { headers: new Headers({ 'Content-type': 'application/json' }) };
+@Injectable()
+export class MovieService {
+  constructor(public http: HttpClient) {}
+  getAll() {
+    return this.http.get(BASE_URL).toPromise();
+  }
+}
--- exe08/server/api/movies/routes/getMovieById.js
+++ exe08/server/api/movies/routes/getMovieById.js
@@ -1,9 +1,9 @@
 const Movie = require('../models/movie');
 module.exports = {
   method: 'GET',
-  path: '/api/movies',
+  path: '/api/movies/{id}',
   handler: async (req, h) => {
-    let movies = await Movie.find();
-    return movies;
+    let movie = await Movie.find({ id: req.params.id });
+    return movie;
   }
 };
