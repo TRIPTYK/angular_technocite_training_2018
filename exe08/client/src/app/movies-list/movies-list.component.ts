5ebeb179ece54713c49d4c9898cd4873086f3866
gilles-bertrand <gilles@triptyk.eu>
2018-02-13 14:55:36 +0100

    add exercice

--- exe08/client/src/app/movies-list/movies-list.component.ts
+++ exe08/client/src/app/movies-list/movies-list.component.ts
@@ -9,8 +9,10 @@ import { Movie } from '../shared/models/movie.model';
 })
 export class MoviesListComponent implements OnInit {
   movies: Movie[];
+  visualMovies: any;
   constructor(public movieService: MovieService) {
     this.movies = [];
+    this.visualMovies = [];
   }
   ngOnInit() {
     this.getMovies();
@@ -18,6 +20,24 @@ export class MoviesListComponent implements OnInit {
   getMovies() {
     this.movieService.getAll().then(result => {
       (result as Movie[]).map(movie => this.movies.push(movie as Movie));
+      this.generateVisualMovies();
+    });
+  }
+  generateVisualMovies() {
+    let tmpArray = [];
+    this.movies.map((element, i) => {
+      if (i % 4 === 0) {
+        if (tmpArray.length > 0) {
+          this.visualMovies.push(tmpArray);
+        }
+        tmpArray = [];
+        tmpArray.push(element);
+      } else {
+        tmpArray.push(element);
+        if (i === this.movies.length - 1) {
+          this.visualMovies.push(tmpArray);
+        }
+      }
     });
   }
 }
